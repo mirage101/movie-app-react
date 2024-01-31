24d8b13b1a2f69055f0b0fdcd6ebefdee1bce9a8
ivan <ivan@stanga.net>
2024-01-31 14:51:09 +0200

    changes in image for logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect } from "react";
-import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from "@mui/material";
-import { Link } from "react-router-dom";
-import { useTheme } from "@mui/styles";
-import useStyles from "./styles";
-import { useGetGenresQuery } from "../../services/TMDB";
-import genreIcons from "../../assets/genres";
-import { useDispatch, useSelector } from "react-redux";
-import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
+import React, { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';
+import useStyles from './styles';
+import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import blueLogo from '../../assets/images/kinoafish-logo.png';
+import redLogo from '../../assets/images/kinoafish-logo-blue.png';
 const categories = [
-  { label: "Popular", value: "popular" },
-  { label: "Top Rated", value: "top_rated" },
-  { label: "Upcoming", value: "upcoming" },
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
 ];
 // const demoCategories = [
 //   { label: "Comedy", value: "comedy" },
@@ -19,8 +21,8 @@ const categories = [
 //   { label: "Animation", value: "animation" },
 // ];
 
-const redLogo = "https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png";
-const blueLogo = "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
+// const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
+// const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 function Sidebar({ setMobileOpen }) {
   const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
@@ -29,13 +31,13 @@ function Sidebar({ setMobileOpen }) {
   const { data, isFetching } = useGetGenresQuery();
   //console.log(genreIdOrCategoryName);
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     setMobileOpen(false);
-  }, [genreIdOrCategoryName])
+  }, [genreIdOrCategoryName]);
   return (
     <>
       <Link to="/" className={classes.imageLink}>
-        <img className={classes.image} src={theme.palette.mode === "light" ? redLogo : blueLogo} alt="Filmpire logo" />
+        <img className={classes.image} src={theme.palette.mode === 'light' ? redLogo : blueLogo} alt="KinoAfish logo" />
       </Link>
       <Divider />
       <List>
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -4,10 +4,10 @@ export default makeStyles((theme) => ({
   imageLink: {
     display: 'flex',
     justifyContent: 'center',
-    padding: '10% 0',
+    padding: '0% 0',
   },
   image: {
-    width: '70%',
+    width: '100%',
   },
   links: {
     color: theme.palette.text.primary,
